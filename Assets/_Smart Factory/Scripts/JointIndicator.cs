6ee41f24bebcd274ee0f06ced049fa3af59dfcde
LiveDimensions <A01197266@itesm.mx>
2023-05-09 14:08:16 -0700

    UR5e export, added LeanTween

---
+++ Assets/_Smart Factory/Scripts/JointIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointIndicator : MonoBehaviour
+{
+    private Vector3 _targetScale;
+
+    public float turnSpeed = 50f;
+    
+    private void Awake()
+    {
+        _targetScale = transform.localScale;
+    }
+
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+        LeanTween.scale(gameObject, _targetScale, 0.5f);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(-Vector3.up * Time.deltaTime * turnSpeed);
+    }
+
+    public void StopIndicating()
+    {
+        LeanTween.scale(gameObject, Vector3.zero, 0.5f).destroyOnComplete = true;
+    }
+    
+}
---
+++ Assets/_Smart Factory/Scripts/UR5e/UR5e_Controller.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class UR5e_Controller : MonoBehaviour
+{
+    [Header("Robot Joints")] [SerializeField]
+    private RobotJoint[] robotJoints;
+
+    [SerializeField] private GameObject jointIndicator;
+
+    private Dictionary<int, JointIndicator> indicators = new Dictionary<int, JointIndicator>();
+
+    private void Update()
+    {
+        foreach (RobotJoint joint in robotJoints)
+        {
+            joint.JointUpdate();
+        }
+    }
+
+    public void IndicateJointPositive(int joint)
+    {
+        IndicateJoint(joint, true);
+    }
+
+    public void IndicateJointNegative(int joint)
+    {
+        IndicateJoint(joint, false);
+    }
+
+    private void IndicateJoint(int joint, bool positive)
+    {
+        if (indicators.ContainsKey(joint)) return;
+
+        JointIndicator indicator = Instantiate(jointIndicator).GetComponent<JointIndicator>();
+        indicator.gameObject.SetActive(true);
+
+        Transform indicatorTransform = indicator.transform;
+        indicatorTransform.SetParent(robotJoints[joint].jointTransform);
+        indicatorTransform.localPosition = Vector3.zero;
+        indicatorTransform.localRotation = Quaternion.identity;
+
+        switch (robotJoints[joint].axis)
+        {
+            case Axis.X:
+                break;
+            case Axis.Y:
+                break;
+            case Axis.Z:
+                indicatorTransform.Rotate(Vector3.right, 90);
+                break;
+        }
+
+        if (positive) indicatorTransform.Rotate(Vector3.right, 180f);
+
+        indicator.turnSpeed = 30f;
+
+        indicators.Add(joint, indicator);
+    }
+
+    public void StopIndicatingJoint(int joint)
+    {
+        if (!indicators.ContainsKey(joint)) return;
+
+        indicators[joint].StopIndicating();
+        indicators.Remove(joint);
+    }
+
+
+    public void PositiveMoveJoint(int joint)
+    {
+        robotJoints[joint].moveValue = 1f;
+    }
+
+    public void NegativeMoveJoint(int joint)
+    {
+        robotJoints[joint].moveValue = -1f;
+    }
+
+    public void StopJoint(int joint)
+    {
+        robotJoints[joint].moveValue = 0;
+    }
+
+    [System.Serializable]
+    private class RobotJoint
+    {
+        public Joint joint = Joint.Base;
+        public Axis axis = Axis.X;
+        public Transform jointTransform;
+        [Range(0f, 50f)] public float turnRate = 10f;
+        [Range(0, 360)] public float minAngle;
+        [Range(0, 360)] public float maxAngle;
+
+        public float moveValue = 0f;
+
+        public void JointUpdate()
+        {
+            if (moveValue == 0) return;
+
+            Vector3 rotDirection = axises[(int) axis];
+            jointTransform.Rotate(rotDirection, Time.deltaTime * moveValue * turnRate, Space.Self);
+
+            Vector3 rotation = jointTransform.localEulerAngles;
+            switch (axis)
+            {
+                case Axis.X:
+                    rotation.x = Mathf.Clamp(rotation.x, minAngle, maxAngle);
+                    break;
+                case Axis.Y:
+                    rotation.y = Mathf.Clamp(rotation.y, minAngle, maxAngle);
+                    break;
+                case Axis.Z:
+                    rotation.z = Mathf.Clamp(rotation.z, minAngle, maxAngle);
+                    break;
+            }
+
+            jointTransform.localEulerAngles = rotation;
+        }
+    }
+
+
+    enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    public static Vector3[] axises = {Vector3.right, Vector3.up, Vector3.forward};
+
+    public enum Joint
+    {
+        Base,
+        Shoulder,
+        Elbow,
+        Wrist1,
+        Wrist2,
+        Wrist3
+    }
+}
